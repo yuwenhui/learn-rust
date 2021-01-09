4153539c1b3955ce6deb0f437ed6cdd073af5cf0
wenhui.yu <winfreyu@gmail.com>
2021-01-10 00:58:35 +0800

    fix: improvement

--- shape-area-with-syntax/src/main.rs
+++ shape-area-with-syntax/src/main.rs
@@ -2,19 +2,19 @@ use std::f64::consts::PI;
 
 fn main() {
   let rectangle = Rectangle {
-    width: 12,
-    height: 12,
+    width: 12_u8,
+    height: 12_u8,
   };
   println!("rectangle area {}", rectangle.area());
 
   let circle = Circle {
-    r: 32.5,
+    r: 32.5_f64,
   };
   println!("circle area {}", circle.area());
 
   let triangle = Triangle {
-    side: 12.5,
-    height: 32.125,
+    side: 12,
+    height: 32,
   };
   println!("triangle area {}", triangle.area());
 }
@@ -43,13 +43,13 @@ impl <T: Copy + std::ops::Mul<Output = T> + std::fmt::Display> Area for Rectangl
   }
 }
 
-impl <T: Copy + std::ops::Mul<Output = f64> + std::fmt::Display> Area for Circle<T> {
+impl <T: Copy + std::ops::Mul<Output = T> + std::fmt::Display + std::convert::Into<f64>> Area for Circle<T> {
   fn area(&self) -> String {
     format!("{}", self.r * self.r * PI)
   }
 }
 
-impl <T: Copy + std::ops::Mul<Output = f64> + std::fmt::Display> Area for Triangle<T> {
+impl <T: Copy + std::ops::Mul<Output = T> + std::fmt::Display + std::convert::Into<f64>> Area for Triangle<T> {
   fn area(&self) -> String {
     format!("{}", self.side * self.height / 2.0)
   }
